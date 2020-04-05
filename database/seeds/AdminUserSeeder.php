c91ab4e5fef0a00f6f25209ecc3a25948e35a1ff
yasminekhaled96 <yasmina.khaled96@gmail.com>
2020-04-05 20:20:32 +0200

    admin seeder

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {  DB::table('admin')->insert([
+        'email' => 'admin@admin.com',
+        'password' => bcrypt('123456'),
+
+    ]);
+    
+    }
+}
