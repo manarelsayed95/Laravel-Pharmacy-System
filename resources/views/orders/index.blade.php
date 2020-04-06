63e0686cb5835ad9edea0edb3c57d29c9102b6f4
MarwaHafiz <marwahafiz1996@gmail.com>
2020-04-06 03:17:03 +0200

    admin layout

---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin_layouts.admin')
+@section('content')
+<div>
+    <h1>orders</h1>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,3 @@ Route::get('/admin', function () {
     return view('admin');
 });
 
-
