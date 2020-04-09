b38ca1e5031a310b3d41bbdee953dc5345b4e1ee
mayar alaa <mayar.alaa188@gmail.com>
2020-04-09 22:58:29 +0200

    adding doctor home page

--- resources/views/doctor.blade.php
+++ resources/views/doctor.blade.php
@@ -1,17 +1,23 @@
-@extends('layouts.auth')
+@extends('layouts.sidebar')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="content">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+     
+  
+    
+          <div class="card card-primary card-outline">
+        
 
-                <div class="card-body">
-                    Welcome,Dear Doctor
-                </div>
-            </div>
-        </div>
-    </div>
+              <div class="card-body">
+                  Welcome,Dear Doctor
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
+
+  </div>
 @endsection
